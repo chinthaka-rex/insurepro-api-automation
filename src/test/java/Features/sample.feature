ffde7d8bf436947bf91d811bf3d069df9aac57b9
chinthaka-rex <cwithanage.c@rex.com>
2022-02-21 22:24:57 +0530

    testrunner implemented 3

--- src/test/java/Features/sample.feature
+++ src/test/java/Features/sample.feature
@@ -1,4 +1,13 @@
-Feature: Print hello world feature
+Feature: Get Json from API
 
-  Scenario: hello world scenario
-    * print 'hello worlds'
+  Scenario: get posts details1
+    Given url 'https://gorest.co.in/public/v2/posts/'
+    And path 9996655
+    When method GET
+    Then status 404
+
+  Scenario: get posts details2
+    Given url 'https://gorest.co.in/public/v2/posts/'
+    And path 2062
+    When method GET
+    Then status 200
--- src/test/java/MyTestRunner/TestRunner.java
+++ src/test/java/MyTestRunner/TestRunner.java
@@ -1,11 +1,23 @@
 package MyTestRunner;
 
-import com.intuit.karate.junit4.Karate;
-import org.junit.runner.RunWith;
-import com.intuit.karate.KarateOptions;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner.Builder;
+import org.junit.Test;
+import util.ExtentReportGen;
 
-@RunWith(Karate.class)
-@KarateOptions(features = "classpath:Features")
+//@RunWith(Karate.class)
+//@KarateOptions(features = "classpath:Features")
 public class TestRunner {
+    @Test
+    public void executeKarateTest() {
+        Builder aRunner = new Builder();
+        aRunner.path("classpath:Features");
+        Results result = aRunner.parallel(1);
 
+        ExtentReportGen extentReport = new ExtentReportGen()
+                .withKarateResult(result)
+                .withReportDir(result.getReportDir())
+                .withReportTitle("CW Test Automation Project");
+        extentReport.generateExtentReport();
+    }
 }
