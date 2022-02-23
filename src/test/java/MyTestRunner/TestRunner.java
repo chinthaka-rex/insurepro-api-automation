5af366cb14c41d80cb235528117c581af92a1429
chinthakaw <chinthaka.wit@gmail.com>
2022-02-23 18:36:06 +0530

    zipping 1

--- src/test/java/MyTestRunner/TestRunner.java
+++ src/test/java/MyTestRunner/TestRunner.java
@@ -5,8 +5,11 @@ import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
 import org.junit.Test;
 import util.ConfigReader;
+import util.CreateZip;
+import util.DeleteFiles;
 import util.ExtentReportGen;
 
+import java.io.IOException;
 import java.util.Properties;
 
 @KarateOptions(
@@ -31,5 +34,18 @@ public class TestRunner {
                 .withReportDir("reports")
                 .withReportTitle(prop.getProperty("extent_report_title"));
         extentReport.generateExtentReport();
+        zippingTheReports();
+    }
+
+    public void zippingTheReports() {
+        DeleteFiles deleteFiles = new DeleteFiles();
+        deleteFiles.deleteThisFiles(prop.getProperty("zip_in"), ".zip");
+
+        CreateZip createZip = new CreateZip();
+        try {
+            createZip.zipTheFile(prop.getProperty("zip_in"), prop.getProperty("zip_out"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
